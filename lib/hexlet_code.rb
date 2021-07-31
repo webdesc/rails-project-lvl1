7b8439ed164b127f5629ddf2c2aca758c51432fe
webdesc webdesc <webdesc@gmail.com>
2021-07-31 15:41:51 +0300

    add tags builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,31 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+    @single_tags = %w[img br input]
+    @pair_tags = %w[a label div span]
+
+    def self.build(name, options = {}, &block)
+      return build_single_tag(name, options) if @single_tags.include? name
+      return build_pair_tag(name, options, &block) if @pair_tags.include? name
+    end
+
+    private
+
+    def self.build_single_tag(name, options)
+      "<#{name}#{create_options(options)}>"
+    end
+
+    def self.build_pair_tag(name, options, &block)
+      "<#{name}#{create_options(options)}>#{block.call}</#{name}>"
+    end
+
+    def self.create_options(options)
+      return "" if options.empty?
+
+      options_array = options.map { |key, value| "#{key}=\"#{value}\"" }
+      " #{options_array.join " "}"
+    end
+  end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -3,11 +3,34 @@
 require "test_helper"
 
 class HexletCodeTest < Minitest::Test
+  def setup
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_create_single_tag_without_attrs
+    expected = '<br>'
+    actual = HexletCode::Tag.build('br')
+    assert_equal expected, actual
+  end
+
+  def test_create_single_tag_with_attrs
+    expected = '<input type="submit" value="Save">'
+    actual = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+    assert_equal expected, actual
+  end
+
+  def test_create_pair_tag_without_attrs
+    expected = '<label>Email</label>'
+    actual = HexletCode::Tag.build('label') { 'Email' }
+    assert_equal expected, actual
+  end
+
+  def test_create_pair_tag_with_attrs
+    expected = '<a href="https://www.jopa.ru" class="link">go</a>'
+    actual = HexletCode::Tag.build('a', href: 'https://www.jopa.ru', class: 'link') { 'go' }
+    assert_equal expected, actual
   end
 end
