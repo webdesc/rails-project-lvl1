57965b5e738222232b645f1f9a47c8a24959afbe
nunin alexey <webdesc@gmail.com>
2021-10-10 23:17:35 +0300

    remove unuse send block

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,8 +7,8 @@ require "hexlet_code/form_template"
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(user, options = {}, &block)
-    form = HexletCode::Form.new(user, options, &block)
+  def self.form_for(user, options = {})
+    form = HexletCode::Form.new(user, options)
     yield(form) if block_given?
     HexletCode::FormTemplate.render(form.config)
   end
